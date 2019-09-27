3a96444a3abc19bb346e39e1cf0d688dfe11e71e
gregjhansell97 <gregjhansell@gmail.com>
2019-09-27 14:24:04 -0400

    restructure: tests shaped/trimmed to run
    The tests don't do anything yet, just getting them to a buildable state
    for further use

---
+++ tests/arduino_compile/arduino_compile.ino
@@ -0,0 +1,17 @@
+#include "hive_map.h"
+
+void setup()
+{
+    Serial.begin(250000);
+}
+
+void serial_write(char* msg, size_t len) {
+    
+}
+void serial_read(char* msg, size_t len) {
+  
+}
+
+void loop()
+{
+}
--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -3,5 +3,4 @@
 #include "hive_map.h"
 
 int main() {
-    assert(HiveMapNode.on_write == NULL);
 }
